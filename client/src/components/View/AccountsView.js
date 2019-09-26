3c690b26e1a2e3364641399de32f46a352c31f19
turuslan <turuslan.devbox@gmail.com>
2019-09-26 19:40:04 +0300

    client: account table paging whole partial

--- client/src/components/View/AccountsView.js
+++ client/src/components/View/AccountsView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import last from 'lodash/last';
 import View from '../Styled/View';
 import ReactTable from '../Styled/Table';
 import AccountView from './AccountView';
@@ -28,26 +29,67 @@ const columns = [
 export class AccountsView extends React.Component {
   state = {
     showAccount: null,
+    page: 0,
+    pages: [null],
   };
+
+  componentWillReceiveProps({ nextAfter }) {
+    const { pages } = this.state;
+    if (last(pages) < nextAfter) {
+      this.setState({ pages: [...pages, nextAfter] });
+    }
+  }
+
+  refetch(variables, state) {
+    this.setState(state);
+    this.props.refetch(variables);
+  }
+  
+  pageToAfter = (page, pageSize) => this.wholePaging ? page * pageSize : this.state.pages[page];
+
+  onPageSizeChange = (pageSize, page) => {
+    if (this.wholePaging) {
+      this.refetch({ after: this.pageToAfter(page, pageSize), pageSize }, { page });
+    } else {
+      this.refetch({ after: null, pageSize }, { page: 0, pages: [null] });
+    }
+  };
+
+  onPageChange = (page) => {
+    this.refetch({ after: this.pageToAfter(page, this.props.pageSize) }, { page });
+  };
+
+  get wholePaging() {
+    return true;
+  }
 
   render() {
     const {
       list,
+      total, pageSize, loading,
     } = this.props;
     const {
       showAccount,
+      page, pages,
     } = this.state;
     return (
       <View>
         <ReactTable
           data={list}
           columns={columns}
-          defaultPageSize={5}
           sortable={false}
-          minRows={0}
+          minRows={1}
           getTrProps={(state, row) => ({
             onClick: () => this.setState({ showAccount: row.original.id }),
           })}
+
+          manual
+          loading={loading}
+          page={page}
+          pageSize={pageSize}
+          onPageChange={this.onPageChange}
+          onPageSizeChange={this.onPageSizeChange}
+          pages={this.wholePaging ? total !== null ? Math.ceil(total / pageSize) : 1 : pages.length}
         />
         <AccountView
           accountId={showAccount}
@@ -60,8 +102,8 @@ export class AccountsView extends React.Component {
 
 export default compose(
   graphql(
-    gql`{
-      list: accountList(count: 100) {
+    gql`query ($pageSize: Int!, $after: Int) {
+      list: accountList(count: $pageSize, after: $after) {
         items {
           id
           quorum
@@ -69,12 +111,24 @@ export default compose(
             name
           }
         }
+        nextAfter
       }
+      total: accountCount
     }`,
     {
-      props({ data: { list } }) {
+      options: {
+        variables: {
+          pageSize: 5,
+        },
+      },
+      props({ data: { list, total = null, loading, refetch, variables: { pageSize } } }) {
         return {
           list: list ? list.items : [],
+          nextAfter: list ? list.nextAfter : null,
+          total,
+          pageSize,
+          loading,
+          refetch,
         };
       },
     },
